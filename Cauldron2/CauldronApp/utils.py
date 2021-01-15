4b636b83206f38b8db5f2ad823913867918ac572
JJMerchante <jj.merchante@gmail.com>
2021-01-15 13:53:02 +0100

    Upgrade pip packages
    Signed-off-by: JJMerchante <jj.merchante@gmail.com>

--- Cauldron2/CauldronApp/utils.py
+++ Cauldron2/CauldronApp/utils.py
@@ -16,12 +16,11 @@ def get_jwt_key(user, backend_roles):
     dirname = os.path.dirname(os.path.abspath(__file__))
     key_location = os.path.join(dirname, 'jwtR256.key')
     with open(key_location, 'r') as f_private:
-        private_key = f_private.read()
-    claims = {
-        "user": user,
-        "roles": backend_roles
-    }
-    return jwt.encode(claims, private_key, algorithm='RS256').decode('utf-8')
+        claims = {
+            "user": user,
+            "roles": backend_roles
+        }
+        return jwt.encode(claims, f_private.read(), algorithm='RS256')
 
 
 def require_authenticated(json_response=True):
